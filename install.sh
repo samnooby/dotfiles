218be3c6c43936828e4f8ec32177bce1fcaac7c9
Sam <samjnewby@hotmail.com>
2021-10-11 13:32:25 -0400

    Adds flags to install script

---
+++ install.sh
@@ -0,0 +1,44 @@
+#The install script for Nooby's i3 arch setup
+
+#Default values that are chosen if no flags are set
+HOMEDIR="/home/$(logname)"
+OLDDIR="$(pwd)/old"
+INSTALLDIR="$(pwd)"
+RESTORE=False
+INSTALL=True
+
+#Gets all the flags from the command line
+while [[ "$1" =~ ^- && ! "$1" == "--" ]]; do case $1 in
+	-h | --help )
+		echo "A install script for Nooby's archlinux i3 setup"
+		echo "Flags: "
+		echo "-h (--help) Get this help message"
+		echo "-d (--dir) Change the home directory(default /home/$(logname))"
+		echo "-r (--restore) Restore config before last install, only valid after installation has happened once"
+		echo "-o (--old-dir) Change the directory that old config is saved to"
+		echo "-n (--no-install) moves all appropriate files but does not install packages"
+		exit
+		;;
+	-d | --dir )
+		shift; HOMEDIR=$1
+		;;
+	-r | --restore )
+		RESTORE=True
+		;;
+	-o | --old-dir )
+		shift; OLDDIR=$1
+		;;
+	-n | --no-install )
+		INSTALL=False
+		;;
+esac; shift; done
+if [[ "$1" == '--' ]]; then shift; fi
+
+#Makes sure all required directories exist
+if [[ ! -d $HOMEDIR ]]; then
+	echo "Could not find $HOMEDIR"
+	exit
+elif [[ ! -d $OLDDIR ]]; then
+	echo "Could not find $OLDDIR"
+	exit
+fi
