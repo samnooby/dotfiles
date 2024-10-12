ac91abe77a7560b9f95f48ac2d80bee5ae6a519e
Sam Newby <samjnewby@hotmail.com>
2024-10-12 15:38:59 -0400

    Adds system and special args to flakes

--- flake.nix
+++ flake.nix
@@ -8,6 +8,8 @@
   outputs = { nixpkgs, ... } @ inputs: 
   {
     nixosConfigurations.default = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
       modules = [
         ./configuration.nix
       ];
