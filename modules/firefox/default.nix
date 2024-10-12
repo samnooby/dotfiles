56a895b98c009c04cda25bb1a1e24a01b7ab3870
Sam Newby <samjnewby@hotmail.com>
2024-10-12 19:47:03 -0400

    Remove again lol

--- modules/firefox/default.nix
+++
@@ -1,9 +0,0 @@
-{ config, pkgs, lib, ... }:
-
-let cfg = config.module.firefox; in {
-  options.module.firefox.enable = lib.mkEnableOption "Enable firefox browser module";
-
-  config = lib.mkIf cfg.enable {
-    programs.firefox.enable = true;
-  };
-}
