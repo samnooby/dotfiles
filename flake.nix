51878cb7beb40294e4ffda8591cfa145f9b1fe63
Sam Newby <samjnewby@hotmail.com>
2024-10-12 14:52:59 -0400

    Adds configuration to flake file

--- flake.nix
+++ flake.nix
@@ -2,14 +2,15 @@
   description = "Sam Newbys personal flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.05";
   };
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+  outputs = { nixpkgs, ... } @ inputs: 
+  {
+    nixosConfigurations.default = nixpkgs.lib.nixosSystem {
+      modules = [
+        ./configuration.nix
+      ];
+    };
   };
 }
