9fe0936ab2173d646bd56e1ddcef1cee778ba6cf
Sam Newby <samjnewby@hotmail.com>
2024-10-13 01:44:15 -0400

    Uses preconfig to set up hyprland

--- modules/window-managers/hyprland/default.nix
+++ modules/window-managers/hyprland/default.nix
@@ -8,31 +8,31 @@ in
 
   config = lib.mkIf cfg.enable {
     programs.kitty.enable = true;
+    wayland.windowManager.hyprland.enable = true;
 
-    wayland.windowManager.hyprland = {
-      enable = true;
-      plugins = [
-        inputs.hyprland-plugins.packages.${pkgs.stdenv.hostPlatform.system}.hyprbars
-        # inputs.hyprland-plugins.packages.${pkgs.system}.hyprbars
-        # inputs.hyprland-plugins.packages.${pkgs.system}.hyprexpo
-      ];
+    wayland.windowManager.hyprland.plugins = [
+      inputs.hyprland-plugins.packages.${pkgs.stdenv.hostPlatform.system}.hyprbars
+    ];
 
-      settings = {
-        "$mod" = "SUPER";
-        bind = [ 
-          "$mod, F, exec, firefox" 
+    wayland.windowManager.hyprland.settings = {
+      "$mod" = "SUPER";
+      bind =
+        [
+          "$mod, F, exec, firefox"
+          ", Print, exec, grimblast copy area"
         ]
         ++ (
+          # workspaces
+          # binds $mod + [shift +] {1..9} to [move to] workspace {1..9}
           builtins.concatLists (builtins.genList (i:
-	    let ws = i + 1;
-	    in [
-	      "$mod, code:1${toString i}, workspace, ${toString ws}"
-	      "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
-	    ]
-          )
-          9)
+              let ws = i + 1;
+              in [
+                "$mod, code:1${toString i}, workspace, ${toString ws}"
+                "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
+              ]
+            )
+            9)
         );
-      };
     };
   };
 }
