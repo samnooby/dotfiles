39768088ef482e55deea0937212246d1a499099f
Sam Newby <samjnewby@hotmail.com>
2024-10-13 01:37:28 -0400

    Hopefully hyprland works

--- modules/window-managers/hyprland/default.nix
+++ modules/window-managers/hyprland/default.nix
@@ -1,4 +1,4 @@
-{ lib, config, pkgs, ... }:
+{ lib, config, pkgs, inputs, ... }:
 
 let
   cfg = config.window-manager.hyprland;
@@ -7,13 +7,32 @@ in
   options.window-manager.hyprland.enable = lib.mkEnableOption "Enable hyprland window manager module";
 
   config = lib.mkIf cfg.enable {
+    programs.kitty.enable = true;
+
     wayland.windowManager.hyprland = {
       enable = true;
-      package = pkgs.hyprland;
-      xwayland.enable = true;
-  
-      # Start hyprland by default
-      systemd.enable = true;
+      plugins = [
+        inputs.hyprland-plugins.packages.${pkgs.stdenv.hostPlatform.system}.hyprbars
+        # inputs.hyprland-plugins.packages.${pkgs.system}.hyprbars
+        # inputs.hyprland-plugins.packages.${pkgs.system}.hyprexpo
+      ];
+
+      settings = {
+        "$mod" = "SUPER";
+        bind = [ 
+          "$mod, F, exec, firefox" 
+        ]
+        ++ (
+          builtins.concatLists (builtins.genList (i:
+	    let ws = i + 1;
+	    in [
+	      "$mod, code:1${toString i}, workspace, ${toString ws}"
+	      "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
+	    ]
+          )
+          9)
+        );
+      };
     };
   };
 }
