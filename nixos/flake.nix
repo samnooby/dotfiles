c8d7f29f75c07a5276ebc8850aab6a83042fe37e
Sam Newby <samjnewby@hotmail.com>
2024-10-13 20:06:09 -0400

    Converts nixos configuration to use home manager

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -3,29 +3,17 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    hyprland.url = "github:hyprwm/Hyprland";
+    
+    sam-home-manager.url = "../home-manager";
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+  outputs = { self, nixpkgs, sam-home-manager, ... }@inputs: {
     nixosConfigurations.default = nixpkgs.lib.nixosSystem {
       specialArgs = {inherit inputs;};
       modules = [
         ./hosts/default/configuration.nix
-        home-manager.nixosModules.home-manager
-        {
-          home-manager = {
-            useGlobalPkgs = true;
-            useUserPackages = true;
-            extraSpecialArgs = { inherit inputs; };
-            users.sam = import ./hosts/default/user.nix;
-          };
-        }
+        
+        sam-home-manager.nixosModules.home-manager
       ];
     };
   };
