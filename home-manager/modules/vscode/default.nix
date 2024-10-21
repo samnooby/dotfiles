353ae908596a5783ec6f0f2320950ff4f9c5faa6
Sam Newby <sam@kahi.io>
2024-10-21 09:49:41 -0400

    Allows only homemanager to affect extensions

--- home-manager/modules/vscode/default.nix
+++ home-manager/modules/vscode/default.nix
@@ -9,6 +9,7 @@ in
   config = lib.mkIf cfg.enable {
     programs.vscode = {
       enable = true;
+      mutableExtensionsDir = false;
       userSettings = {
         "debug.toolBarLocation" = "docked";
 
