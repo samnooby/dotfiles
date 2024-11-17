168fd5ffadb3cda4d2bc2f0ad2352723e40c18de
Sam Newby <samjnewby@hotmail.com>
2024-11-17 18:28:39 -0500

    Remove fish for now

--- hosts/wsl/configuration.nix
+++ hosts/wsl/configuration.nix
@@ -49,27 +49,6 @@ with builtins;
   environment.systemPackages = [
    pkgs.gcc14
   ];
-
-  services.dbus.enable = true;
-  system.activationScripts.createDirectory = ''
-    mkdir -p /run/user/1000/
-    chown 1000:1001 /run/user/1000
-    chmod 700 /run/user/1000
-  '';
-
-  systemd.user.services.dbus-session = {
-    description = "D-Bus Session Bus for WSL";
-    serviceConfig = {
-      ExecStart = "${pkgs.dbus}/bin/dbus-daemon --session --address=unix:path=%t/bus --nofork --nopidfile --syslog-only";
-      ExecStop = "${pkgs.dbus}/bin/dbus-send --session --type=method_call --dest=org.freedesktop.DBus / org.freedesktop.DBus.LeaveSession";
-    };
-    wantedBy = [ "default.target" ];
-  };
-
-  environment.variables = {
-    XDG_RUNTIME_DIR = "/run/user/1000";
-    DBUS_SESSION_BUS_ADDRESS = "unix:path=${toString (getEnv "XDG_RUNTIME_DIR")}/bus";
-  };
   
   system.stateVersion = "24.05"; # Did you read the comment?
 }
--- modules/wayland.nix
+++ modules/wayland.nix
@@ -1,6 +1,28 @@
-{ username, config, lib, ... }:
+{ pkgs, username, config, lib, ... }:
+with builtins;
 
 {
+  services.dbus.enable = true;
+  system.activationScripts.createDirectory = ''
+    mkdir -p /run/user/1000/
+    chown 1000:1001 /run/user/1000
+    chmod 700 /run/user/1000
+  '';
+
+  # systemd.user.services.dbus-session = {
+  #  description = "D-Bus Session Bus for WSL";
+  #  serviceConfig = {
+  #    ExecStart = "${pkgs.dbus}/bin/dbus-daemon --session --address=unix:path=%t/bus --nofork --nopidfile --syslog-only";
+  #    ExecStop = "${pkgs.dbus}/bin/dbus-send --session --type=method_call --dest=org.freedesktop.DBus / org.freedesktop.DBus.LeaveSession";
+  #  };
+  #  wantedBy = [ "default.target" ];
+  #};
+
+  environment.variables = {
+    XDG_RUNTIME_DIR = "/run/user/1000";
+    DBUS_SESSION_BUS_ADDRESS = "unix:path=${toString (getEnv "XDG_RUNTIME_DIR")}/bus";
+  };
+
   programs.sway.enable = true;
 
   home-manager.users.${username} = { lib, config, ... }: {
@@ -8,10 +30,10 @@
     #   XDG_RUNTIME_DIR = "${config.home.homeDirectory}/.run";
     # };
     
-    # home.activation = {
-    #  createRunDirectory = lib.hm.dag.entryAfter ["writeBoundary"] ''
-    #    mkdir -p "${config.home.homeDirectory}/.run"
-    #  '';
-    # };
+    home.activation = {
+     startDbus = lib.hm.dag.entryAfter ["sessionVariables"] ''
+        eval $(dbus-launch)
+     '';
+    };
   };
 }
