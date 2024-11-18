13a61dd627e40244d6172875b7640e95cf9f21cf
Sam Newby <samjnewby@hotmail.com>
2024-11-18 10:33:32 -0500

    Removes treesitter

--- modules/neovim/default.nix
+++ modules/neovim/default.nix
@@ -1,40 +1,40 @@
 { username, pkgs, ... }:
 
 let
-  treesitterWithGrammars = (
-    pkgs.vimPlugins.nvim-treesitter.withPlugins (p: [
-      p.bash
-      p.comment
-      p.css
-      p.dockerfile
-      p.fish
-      p.gitattributes
-      p.gitignore
-      p.go
-      p.gomod
-      p.gowork
-      p.hcl
-      p.javascript
-      p.jq
-      p.json5
-      p.json
-      p.lua
-      p.make
-      p.markdown
-      p.nix
-      p.python
-      p.rust
-      p.toml
-      p.typescript
-      p.vue
-      p.yaml
-    ])
-  );
-  treesitter-parsers = pkgs.symlinkJoin {
-    name = "treesitter-parsers";
-    paths = treesitterWithGrammars.dependencies;
-  };
 in
+# treesitterWithGrammars = (
+#   pkgs.vimPlugins.nvim-treesitter.withPlugins (p: [
+#     p.bash
+#     p.comment
+#       p.css
+#       p.dockerfile
+#       p.fish
+#       p.gitattributes
+#       p.gitignore
+#       p.go
+#       p.gomod
+#       p.gowork
+#       p.hcl
+#       p.javascript
+#       p.jq
+#       p.json5
+#       p.json
+#       p.lua
+#       p.make
+#       p.markdown
+#       p.nix
+#       p.python
+#       p.rust
+#       p.toml
+#       p.typescript
+#       p.vue
+#       p.yaml
+#     ])
+#   );
+# treesitter-parsers = pkgs.symlinkJoin {
+#   name = "treesitter-parsers";
+#   paths = treesitterWithGrammars.dependencies;
+# };
 {
   home-manager.users.${username} = {
     home.packages = with pkgs; [
@@ -58,7 +58,7 @@ in
         require("config.lazy")
       '';
       plugins = [
-        treesitterWithGrammars
+        # treesitterWithGrammars
       ];
     };
     home.file."./.config/nvim/" = {
@@ -66,10 +66,10 @@ in
       recursive = true;
     };
 
-    home.file."./local/share/nvim/nix/nvim-treesitter/" = {
-      source = treesitterWithGrammars;
-      recursive = true;
-    };
+    # home.file."./local/share/nvim/nix/nvim-treesitter/" = {
+    #  source = treesitterWithGrammars;
+    #  recursive = true;
+    # };
   };
 
   environment.variables.EDITOR = "nvim";
