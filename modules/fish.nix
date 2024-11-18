5cc1e46e8d3a056b8636db119fa7b30d6fc2ae89
Sam Newby <samjnewby@hotmail.com>
2024-11-18 00:18:40 -0500

    Add tide to fish

--- modules/fish.nix
+++ modules/fish.nix
@@ -3,28 +3,24 @@
 {
   programs.fish = {
     enable = true;
-    loginShellInit = '' 
-      if not set -q DBUS_SESSION_BUS_ADDRESS
-        # Launch DBus and capture output
-        set new_vars (dbus-launch --exit-with-session)
-        
-        # Loop through each line of output
-        for line in $new_vars
-          # Split each line into key and value at the first '='
-          set split_var (string split -m1 "=" $line)
-
-          # Ensure both key and value exist before setting as environment variable
-          if test (count $split_var) -eq 2
-              set -Ux $split_var[1] $split_var[2]
-          end
-        end
-      end
-    '';
-    promptInit = ''
-      function fish_prompt
-        string join "" -- (set_color green) $PWD (set_color normal) '>'
-      end
-    '';
   };
   users.users.${username}.shell = pkgs.fish;
+
+  environment.systemPackages = [
+    pkgs.screenfetch
+  ];
+
+  home-manager.users.${username} = {
+    programs.fish = {
+      enable = true;
+      interactiveShellInit = ''
+        function fish_greeting
+          screenfetch
+        end
+      '';
+      plugins = [
+        { name = "tide"; src = pkgs.fishPlugins.tide.src; }
+      ];
+    };
+  };
 }
