fec46732aa1aec01b2e3cccc8ad8543a38330d54
Sam Newby <samjnewby@hotmail.com>
2024-11-18 04:53:09 -0500

    Adds stylua package

--- modules/neovim/default.nix
+++ modules/neovim/default.nix
@@ -1,33 +1,35 @@
 { username, pkgs, ... }:
 
 let
-  treesitterWithGrammars = (pkgs.vimPlugins.nvim-treesitter.withPlugins (p: [
-    p.bash
-    p.comment
-    p.css
-    p.dockerfile
-    p.fish
-    p.gitattributes
-    p.gitignore
-    p.go
-    p.gomod
-    p.gowork
-    p.hcl
-    p.javascript
-    p.jq
-    p.json5
-    p.json
-    p.lua
-    p.make
-    p.markdown
-    p.nix
-    p.python
-    p.rust
-    p.toml
-    p.typescript
-    p.vue
-    p.yaml
-  ]));
+  treesitterWithGrammars = (
+    pkgs.vimPlugins.nvim-treesitter.withPlugins (p: [
+      p.bash
+      p.comment
+      p.css
+      p.dockerfile
+      p.fish
+      p.gitattributes
+      p.gitignore
+      p.go
+      p.gomod
+      p.gowork
+      p.hcl
+      p.javascript
+      p.jq
+      p.json5
+      p.json
+      p.lua
+      p.make
+      p.markdown
+      p.nix
+      p.python
+      p.rust
+      p.toml
+      p.typescript
+      p.vue
+      p.yaml
+    ])
+  );
   treesitter-parsers = pkgs.symlinkJoin {
     name = "treesitter-parsers";
     paths = treesitterWithGrammars.dependencies;
@@ -41,6 +43,7 @@ in
       cargo
       unzip
       nixfmt-rfc-style
+      stylua
     ];
 
     programs.neovim = {
