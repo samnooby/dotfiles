09177cd489383dfc30da03cdd86c8d495b3a69fc
Sam Newby <samjnewby@hotmail.com>
2024-11-18 15:00:13 -0500

    Adds neovim plugins to pkgs

--- modules/neovim/config/lua/plugins/disabled.lua
+++ modules/neovim/config/lua/plugins/disabled.lua
@@ -5,9 +5,16 @@ return {
       colorscheme = function() end
     }
   },
+  -- Disable theming, controled by stylix
+  { "folke/lazydev.nvim", enabled = false },
   { "folke/tokyonight.nvim", enabled = false },
   { "catppuccin", enabled = false },
+  -- Disable mason package manager, instead using nix
   { "williamboman/mason.nvim", enabled = false },
   { "williamboman/mason-lspconfig.nvim", enabled = false },
   { "jay-babu/mason-nvim-dap.nvim", enabled = false },
+  -- Seems to only add support for a luvit package
+  { "Bilal2453/luvit-meta", enabled = false },
+  -- Treesitter html and jsx plugin (Useless for now)
+  { "windwp/nvim-ts-autotag", enabled = false },
 }
--- modules/neovim/default.nix
+++ modules/neovim/default.nix
@@ -4,10 +4,20 @@
   config,
   ...
 }:
+
+let
+  treesitterWithGrammers = (
+    pkgs.vimPlugins.nvim-treesitter.withPlugins(p: [
+      p.fish
+      p.comment
+    ])
+  );
+in
 {
   home-manager.users.${username} = {
     home.packages = with pkgs; [
       nodejs_22
+      gcc14
     ];
 
     programs.neovim = {
@@ -18,9 +28,57 @@
       withNodeJs = true;
 
       plugins = with pkgs.vimPlugins; [
+        # Base lazyvim pkgs
         LazyVim
         lazy-nvim
+
+        # Coding pkgs
+        cmp-nvim-lsp
+        cmp-path
+        cmp-buffer
+        nvim-cmp
+        nvim-snippets
+        friendly-snippets
+        mini-pairs
+        mini-ai
+        ts-comments-nvim
+        
+        # Editor pkgs
+        neo-tree-nvim
+        grug-far-nvim
+        flash-nvim
+        gitsigns-nvim
+        trouble-nvim
+        todo-comments-nvim
+
+        # Formatting
+        conform-nvim
+
+        # Linting
+        nvim-lint
+
+        # LSP
+        nvim-lspconfig
+        neoconf-nvim
+        none-ls-nvim
+
+        # Treesitter
         which-key-nvim
+        nvim-treesitter.withAllGrammars
+        nvim-treesitter-textobjects
+
+        # UI
+        bufferline-nvim
+        lualine-nvim
+        indent-blankline-nvim
+        noice-nvim
+        mini-icons
+        nui-nvim
+        dashboard-nvim
+
+        # Util
+        persistence-nvim
+        plenary-nvim
       ];
 
       extraLuaConfig = ''
@@ -43,13 +101,15 @@
             missing = false,
           },
           spec = {
-	    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+	          { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+            { import = "lazyvim.plugins.extras.lsp.neoconf" },
+            { import = "lazyvim.plugins.extras.lsp.none-ls" },
             { import = "plugins" },
           },
-	  defaults = {
-	    lazy = false,
-	    version = false,
-	  },
+	        defaults = {
+	          lazy = false,
+	          version = false,
+	        },
         })
       '';
     };
