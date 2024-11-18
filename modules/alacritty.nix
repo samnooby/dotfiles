85eaf9961dce8264cb7c05c97c2c8a5e16c288ed
Sam Newby <samjnewby@hotmail.com>
2024-11-17 23:48:49 -0500

    Convert alacritty to home-manager

--- modules/alacritty.nix
+++ modules/alacritty.nix
@@ -1,11 +1,18 @@
 { username, pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs; [
-    alacritty
-  ];
-
   home-manager.users.${username} = {
+    programs.alacritty = {
+      enable = true;
+      settings = {
+        window = {
+          decorations = "None";
+          startup_mode = "Fullscreen";
+        };
+        terminal.shell = "fish";
+      };
+    };
+
     home.sessionVariables = {
       TERMINAL = "alacritty";
       TERM = "alacritty";
