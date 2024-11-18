2e1d39e392119fb7948c7d6ff7ab13d9597ad24c
Sam Newby <samjnewby@hotmail.com>
2024-11-17 23:09:56 -0500

    Remove old display managers

--- modules/i3.nix
+++ modules/i3.nix
@@ -1,6 +1,11 @@
 { pkgs, username, ... }:
 
 {
+  # disable all other display managers
+  services.xserver.displayManager.lightdm.enable = false;
+  services.xserver.displayManager.gdm.enable = false;
+  services.xserver.displayManager.sddm.enable = false;
+
   services.xserver = {
     enable = true;
     windowManager.i3.enable = true;
