a15aef4be96fc0cccccc2d8e13bcdd0b945d372a
Sam Newby <samjnewby@hotmail.com>
2024-11-17 23:33:59 -0500

    Start dbus in fish configuration

--- modules/fish.nix
+++ modules/fish.nix
@@ -4,6 +4,21 @@
   programs.fish = {
     enable = true;
     loginShellInit = '' 
+      if not set -q DBUS_SESSION_BUS_ADDRESS
+        # Launch DBus and capture output
+        set new_vars (dbus-launch)
+        
+        # Loop through each line of output
+        for line in $new_vars
+          # Split each line into key and value at the first '='
+          set split_var (string split -m1 "=" $line)
+
+          # Ensure both key and value exist before setting as environment variable
+          if test (count $split_var) -eq 2
+              set -Ux $split_var[1] $split_var[2]
+          end
+        end
+      end
     '';
     promptInit = ''
       function fish_prompt
