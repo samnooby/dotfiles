eebc1dfe2fd76ae4879c3bb9cbf15e84e228d37c
sam <samjnewby@hotmail.com>
2024-12-04 13:44:29 -0500

    Add function to navigate to seperate projects

--- modules/fish.nix
+++ modules/fish.nix
@@ -64,6 +64,20 @@
             echo "Usage: cdl <target directory>"
           end
         '';
+        p = ''
+          if test -z $argv
+            echo "Usage: p <project name>"
+            return 1
+          end
+
+          set project_dir ~/Projects/$argv[1]
+          if test -d $project_dir
+            cd $project_dir
+          else
+            echo "Project not found: $argv[1]"
+            return 1
+          end
+        '';
       };
       shellInit =
         "tide configure --auto --style=Rainbow --prompt_colors='16 colors' --show_time='12-hour format' --rainbow_prompt_separators=Slanted --powerline_prompt_heads=Slanted --powerline_prompt_tails=Flat --powerline_prompt_style='Two lines, character and frame' --prompt_connection=Disconnected --powerline_right_prompt_frame=No --prompt_spacing=Sparse --icons='Many icons' --transient=No\n      ";
