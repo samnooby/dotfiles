edc1eb948a0eaab697cd1ef1d8d6640a901d9b89
Sam <samjnewby@hotmail.com>
2025-01-28 10:21:30 -0500

    Adds typescript support

--- modules/neovim/config/lua/config/lazy.lua
+++ modules/neovim/config/lua/config/lazy.lua
@@ -22,6 +22,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.nix" },
     { import = "lazyvim.plugins.extras.lang.sql" },
     { import = "lazyvim.plugins.extras.lang.python" },
+    { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.ai.copilot" },
     { import = "lazyvim.plugins.extras.ai.copilot-chat" },
     { import = "plugins" },
